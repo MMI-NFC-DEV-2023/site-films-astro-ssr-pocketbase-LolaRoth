e83471214a68fb61e942e738d71f2a931ca22057
Lola Roth <lola.roth@edu.univ-fcomte.fr>
2025-02-18 13:34:09 +0100

    update PocketBase URL to use port 1803 in backend and middleware

--- src/backend.ts
+++ src/backend.ts
@@ -1,4 +1,4 @@
 import PocketBase from "pocketbase";
 import type { TypedPocketBase } from "./pocketbase-typegen";
 
-export const pb = new PocketBase("http://127.0.0.1:8090") as TypedPocketBase;
+export const pb = new PocketBase("http://127.0.0.1:1803") as TypedPocketBase;
