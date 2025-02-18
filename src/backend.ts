8b3511f35c4b399fac3bb5c1e7556ff2d5c33cab
Lola Roth <lola.roth@edu.univ-fcomte.fr>
2025-02-18 13:43:57 +0100

    update PocketBase initialization to use environment variable for URL

--- src/backend.ts
+++ src/backend.ts
@@ -1,4 +1,6 @@
 import PocketBase from "pocketbase";
 import type { TypedPocketBase } from "./pocketbase-typegen";
 
-export const pb = new PocketBase("http://127.0.0.1:1803") as TypedPocketBase;
+export const pb = new PocketBase(
+  import.meta.env.VITE_URL_POCKETBASE
+) as TypedPocketBase;
